4bf77f8e8b18d7bf386eed53fff48830b1dc5a11
Doug Morand <dmorand@gmail.com>
2025-04-15 19:08:29 -0400

    feat: add check for alpine-git image existence and auto-build

--- script.sh
+++ script.sh
@@ -2,6 +2,15 @@
 
 set -e
 
+# Check if alpine-git image exists
+if ! docker images alpine-git | grep -q alpine-git; then
+    echo "alpine-git image does not exist, building it..."
+    # Change to script directory relative to this file even if its a symlink
+    pushd "$(dirname "$(readlink -f "$0")")" &> /dev/null
+    docker build -t alpine-git .
+    popd &> /dev/null
+fi
+
 # Run git inside container
 docker run --rm -it \
     -v $HOME/.ssh/id_ecdsa_github:/home/git-user/.ssh/id_rsa \
